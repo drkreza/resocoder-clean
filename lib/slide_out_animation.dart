39251c273dcd8bfc70e99dccf2f76656ba0b0faa
r.derikvand <reza.derikvand@gmail.com>
2023-10-21 16:19:50 +0330

    create slide animation,left to right and right to left

---
+++ lib/slide_out_animation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: SlideOutAnimation(),
+  ));
+}
+
+class SlideOutAnimation extends StatefulWidget {
+  const SlideOutAnimation({super.key});
+
+  @override
+  SlideOutAnimationState createState() => SlideOutAnimationState();
+}
+
+class SlideOutAnimationState extends State<SlideOutAnimation> {
+  double _position = 0.0;
+  final Duration _duration = const Duration(seconds: 2);
+
+  @override
+  void initState() {
+    super.initState();
+    startAnimation();
+  }
+
+  void startAnimation() {
+    Future.delayed(_duration, () {
+      setState(() {
+        _position = -MediaQuery.of(context).size.width;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Slide Out Animation'),
+      ),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: _duration,
+            right: _position,
+            child: Container(
+              width: 200,
+              height: 100,
+              color: Colors.blue,
+              child: const Center(
+                child: Text(
+                  'Slide Out',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: _duration,
+            left: _position,
+            child: Container(
+              width: 200,
+              height: 100,
+              color: Colors.blue,
+              child: const Center(
+                child: Text(
+                  'Slide Out',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
